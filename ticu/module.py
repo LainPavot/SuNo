8a5d07cddd738e9fb573a480cd541d9a7e96ca69
lain <lain.pavot@protonmail.com>
2022-03-21 00:08:04 +0100

    feat(module:help): add help display functions

--- ticu/module.py
+++ ticu/module.py
@@ -30,3 +30,46 @@ class TiCuModule:
 
   def __hash__(self):
     return hash(f"TiCuModule-{self.name}")
+
+  async def _command_help(self, message, command, args):
+    await self.send_message(
+      message.channel,
+      self._build_module_md_help()
+    )
+
+  async def _command_all_helps(self, message):
+    await self.send_message(
+      message.channel,
+      self._build_all_modules_md_help()
+    )
+
+  def _build_module_md_help(self):
+    return "\n".join((
+      f"```markdown",
+      self._build_module_raw_help(),
+      f"```",
+    ))
+
+  def _build_all_modules_md_help(self):
+    return "\n".join((
+      "```markdown",
+      "\n".join(
+        module._build_module_raw_help()
+        for module in self._app._modules
+      ),
+      "```",
+    ))
+
+  def _build_module_raw_help(self):
+    commands_help = "\n".join(
+      f"{name}\n---\n{self.command_info[name].get('help', '')}\n"
+      for name in self.command_info
+    )
+    return "\n".join((
+      f"{self.name}",
+      f"===",
+      f"{self.module_help}",
+      "\nCommandes:\n * "+"\n * ".join(command for command in self.command_info),
+      f"",
+      f"{commands_help}",
+    ))
